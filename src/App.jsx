b7e3975151a1e69fed142b76bc4b6c32aaef0722
Mh-Monzil <monzil246@gmail.com>
2024-07-29 12:57:02 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <main className='h-screen w-full bg-[#111] p-5'>
+      <div className="box h-[300px] w-[300px] bg-[crimson] rounded-[10px]">
+
+      </div>
+    </main>
+  );
+};
+
+export default App;
