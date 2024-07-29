fe77a838fd614952eeb900bb5e2008fd3c406d23
Mh-Monzil <monzil246@gmail.com>
2024-07-29 13:04:46 +0600

    box amination done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import { useGSAP } from "@gsap/react";
+import gsap from 'gsap';
 
 const App = () => {
+
+  useGSAP(() => {
+    gsap.to(".box", {
+      x: 600,
+      delay: 2,
+      duration: 1,
+      borderRadius: "50%",
+    })
+  })
+
   return (
     <main className='h-screen w-full bg-[#111] p-5'>
       <div className="box h-[300px] w-[300px] bg-[crimson] rounded-[10px]">
